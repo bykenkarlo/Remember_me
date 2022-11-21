9e5e4ffe34c2680f088f6873f90c4b85a901c30e
Ken Karlo <20103755+bykenkarlo@users.noreply.github.com>
2022-11-21 17:42:52 +0800

    Update js_script.js

--- js_script.js
+++ js_script.js
@@ -2,3 +2,11 @@
 var find = ['<span style="font-weight: 400;">', '</span>'];
 var replace = ["", ""];
 content = content.replace(new RegExp("(" + find.map(function(i){return i.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&")}).join("|") + ")", "g"), function(s){ return replace[find.indexOf(s)]});
+
+
+// Select2 in Bootstrap modal
+$(document).ready(function() {
+  $('.select2').select2({
+    dropdownParent: $('#modal_id .modal-content')
+  });
+});
